9d605ca32db2da213f8a955970e463f0fe5b0ce1
god <prajbhan@andrew.cmu.edu>
2023-06-21 12:13:23 -0400

    removed pymaze

---
+++ evolution/maze_experiment.py
@@ -0,0 +1,55 @@
+from src.network_blimps import *
+from pymaze.src.maze_manager import MazeManager
+from pymaze.src.maze import Maze
+
+class aMazeBlimp(xyBlimp):
+    def __init__(self,
+                 num_agents,
+                 start_zone,
+                 scenePath,
+                 blimpPath,
+                 networkfn,
+                 height_range,
+                 use_ultra,
+                 height_factor=.2,
+                 sim=None,
+                 simId=23000,
+                 msg_queue=10,
+                 wakeup=None,
+                 sleeptime=.01,
+                 spawn_tries=100):
+        """
+
+
+        @param num_agents: number of blimps in this swarm expiriment
+        @param start_zone: int -> (RxR U R)^3 goes from the blimp number to the spawn area
+                (each dimension could be (value) or (low, high), chosen uniformly at random)
+        @param scenePath: path to coppeliasim scene
+        @param blimpPath: path to blimp for spawning
+        @param networkfn: neural network function call for blimp to act
+        @param height_range: R^2, height range to keep blimps at
+        @param use_ultra: whether to use ultrasound to set height (and as network input)
+        @param height_factor: factor to multiply height adjust by
+        @param sim: simulator, if already defined
+        @param simId: simulator id, used to pass messages to correct topics
+        @param msg_queue: queue length of ROS messages
+        @param wakeup: code to run in command line before starting experiment
+        @param sleeptime: time to wait before big commands (i.e. stop simulation, start simulation, pause simulation)
+        @param spawn_tries: number of tries to spawn without collisions before giving up
+                if 1, then sets position, does not change if collision detected
+        """
+        super().__init__(
+            num_agents=num_agents,
+            start_zone=start_zone,
+            scenePath=scenePath,
+            blimpPath=blimpPath,
+            networkfn=networkfn,
+            height_range=height_range,
+            use_ultra=use_ultra,
+            height_factor=height_factor,
+            sim=sim,
+            simId=simId,
+            msg_queue=msg_queue,
+            wakeup=wakeup,
+            sleeptime=sleeptime,
+            spawn_tries=spawn_tries)
