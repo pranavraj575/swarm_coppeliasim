3a47b993bbd1c3fa785823633979d09e6c90966c
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2023-06-29 22:43:22 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='blimp_coppeliasim',
+    version='6.9.0',
+    packages=find_packages(),
+    license='Liscence to Krill',
+)
