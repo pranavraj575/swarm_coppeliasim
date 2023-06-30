1e200214d04b0e10d6d9aa1fa901c66e84f3186b
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2023-06-30 19:09:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ setup(
     name='blimp_coppeliasim',
     version='6.9.0',
     packages=find_packages(),
+    install_requires=['numpy',
+                      'psutil',
+                      'zmq',
+                      'cbor',
+                      'neat-python==0.92',
+                     ]
     license='Liscence to Krill',
 )
