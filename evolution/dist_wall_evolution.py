70c55d33600039b28660457023d7c564508e156d
god <prajbhan@andrew.cmu.edu>
2023-07-06 11:52:02 -0400

    distance sensing wall evolution

---
+++ evolution/dist_wall_evolution.py
@@ -0,0 +1,113 @@
+from src.network_blimps import *
+from evolution.evolutionBlimp import EvolutionExperiment
+import argparse
+
+parser = argparse.ArgumentParser(description="for creating and running wall climbing evolutionary experiments")
+parser.add_argument("-a", "--agents", type=int, required=False, default=20,
+                    help="Specify number of agents")
+parser.add_argument("-g", "--generations", type=int, required=False, default=0,
+                    help="generations to train for")
+parser.add_argument("--create", action="store_true", required=False,
+                    help="whether to create new directory")
+
+parser.add_argument("--height_lower", type=float, required=False, default=.8,
+                    help="lower bound of height to hold blimps at")
+parser.add_argument("--height_upper", type=float, required=False, default=1.2,
+                    help="upper bound of height to hold blimps at")
+
+parser.add_argument("--xmin", type=float, required=False, default=.5,
+                    help="x spawning lower bound (should be > 0 so they do not spawn on other side of wall)")
+parser.add_argument("--xmax", type=float, required=False, default=8.,
+                    help="x spawning upper bound")
+
+parser.add_argument("--ymin", type=float, required=False, default=-4.,
+                    help="y spawning upper bound")
+parser.add_argument("--ymax", type=float, required=False, default=4.,
+                    help="y spawning upper bound")
+
+parser.add_argument("--zmin", type=float, required=False, default=.8,
+                    help="z spawning upper bound")
+parser.add_argument("--zmax", type=float, required=False, default=1.2,
+                    help="z spawning upper bound")
+
+parser.add_argument("--num_sims", type=int, required=False, default=8,
+                    help="number of simulators to use for training")
+parser.add_argument("--sims_low", type=int, required=False, default=-1,
+                    help="low bound of num_sims to try")
+parser.add_argument("--sims_high", type=int, required=False, default=-1,
+                    help="high bound of num_sims to try")
+parser.add_argument("--offset", type=int, required=False, default=0,
+                    help="offset port number (should be number of simulators already in use)")
+parser.add_argument("--port_step", type=int, required=False, default=2,
+                    help="ports to skip for each new coppeliasim instance")
+parser.add_argument("--overwrite", action="store_true", required=False,
+                    help="whether to overwrite start instead of starting at recent checkpoint")
+parser.add_argument("--show", action="store_true", required=False,
+                    help="whether to show stats of all gens")
+parser.add_argument("--show_result", action="store_true", required=False,
+                    help="whether to show result at end")
+args = parser.parse_args()
+AGENTS = args.agents
+gens = args.generations
+if args.sims_low >= 1:
+    if not args.sims_low <= args.num_sims or not args.num_sims < args.sims_high:
+        raise Exception("bruh")
+END = 60
+h_low = args.height_lower
+h_upp = args.height_upper
+
+
+def SPAWN_ZONE(i):
+    return (args.xmin, args.xmax), (args.ymin, args.ymax), (args.zmin, args.zmax)
+
+
+def expe_make(net, sim=None, port=23000, wakeup=None):
+    return dist_sense_xy_wall_climb_blimp(num_agents=AGENTS,
+                               start_zone=SPAWN_ZONE,
+                               scenePath=caged_wall_climb_path,
+                               blimpPath=narrow_blimp_path,
+                               networkfn=net.activate,
+                               end_time=END,
+                               height_range=(h_low, h_upp),
+                               use_ultra=True,
+                               height_factor=1.,
+                               sim=sim,
+                               simId=port,
+                               wakeup=wakeup,
+                               sleeptime=.01
+                               )
+
+
+save_name = str(AGENTS) + '_blimp_height_' + str(h_low).replace('.', '_') + "_to_" + str(h_upp).replace('.', '_') + \
+            '_wall_climb_neighbor_dist_sensing'
+checkpt_dir = os.path.join(DIR, 'checkpoints', save_name)
+print("SAVING TO:", checkpt_dir)
+
+if not os.path.exists(checkpt_dir):
+    if args.create:
+        os.makedirs(checkpt_dir)
+    else:
+        raise Exception("DIRECTORY DOES NOT EXIST (try running with --create): " + checkpt_dir)
+ee = EvolutionExperiment(checkpt_dir=checkpt_dir,
+                         exp_maker=expe_make,
+                         config_name='blimp_wall')
+if gens:
+    port_step = args.port_step
+    zmq_def_port = 23000 + port_step*args.offset
+    websocket_def_port = 23050 + port_step*args.offset
+
+    ee.train(generations=gens,
+             TRIALS=1,
+             num_simulators=args.num_sims,
+             headless=not args.show,
+             restore=not args.overwrite,
+             evaluate_each_gen=True,
+             zmq_def_port=zmq_def_port,
+             websocket_def_port=websocket_def_port,
+             port_step=port_step,
+             num_sim_range=None if args.sims_low < 1 else (args.sims_low, args.sims_high)
+             )
+if args.show:
+    ee.show_stats()
+if args.show_result:
+    print(ee.result_of_experiment(gen_indices=[0, 1, 2, 3]))
--- src/network_blimps.py
+++ src/network_blimps.py
@@ -471,6 +471,80 @@ class xy_wall_climb_blimp(xyBlimp):
         return self.sim.getSimulationTime() > self.end_time
 
 
+class dist_sense_xy_wall_climb_blimp(xyBlimp):
+    def __init__(self,
+                 num_agents,
+                 start_zone,
+                 scenePath,
+                 blimpPath,
+                 networkfn,
+                 height_range,
+                 use_ultra,
+                 end_time,
+                 height_factor=.2,
+                 sim=None,
+                 simId=23000,
+                 msg_queue=10,
+                 wakeup=None,
+                 sleeptime=.01,
+                 spawn_tries=100):
+        """
+        blimp only sees xy coordinates, rewarded for mean closeness to origin
+
+        @param num_agents: number of blimps in this swarm expiriment
+        @param start_zone: int -> (RxR U R)^3 goes from the blimp number to the spawn area
+                (each dimension could be (value) or (low, high), chosen uniformly at random)
+        @param scenePath: path to coppeliasim scene
+        @param blimpPath: path to blimp for spawning
+        @param networkfn: neural network function call for blimp to act
+        @param height_range: R^2, height range to keep blimps at
+        @param use_ultra: whether to use ultrasound to set height (and as network input)
+        @param end_time: time it takes for experiment to end
+        @param rng: range to detect neighbors
+        @param height_factor: factor to multiply height adjust by
+        @param sim: simulator, if already defined
+        @param simId: simulator id, used to pass messages to correct topics
+        @param msg_queue: queue length of ROS messages
+        @param wakeup: code to run in command line before starting experiment
+        @param sleeptime: time to wait before big commands (i.e. stop simulation, start simulation, pause simulation)
+        @param spawn_tries: number of tries to spawn without collisions before giving up
+                if 1, then sets position, does not change if collision detected
+        """
+        super().__init__(
+            num_agents=num_agents,
+            start_zone=start_zone,
+            scenePath=scenePath,
+            blimpPath=blimpPath,
+            networkfn=networkfn,
+            height_range=height_range,
+            use_ultra=use_ultra,
+            height_factor=height_factor,
+            sim=sim,
+            simId=simId,
+            msg_queue=msg_queue,
+            wakeup=wakeup,
+            sleeptime=sleeptime,
+            spawn_tries=spawn_tries)
+        self.end_time = end_time
+
+    ####################################################################################################################
+    # network functions
+    ####################################################################################################################
+    def get_network_input(self, agent_id):
+        """
+        gets the network input for agent specified
+
+        @param agent_id: agent to get input for
+        @return: R^(l*k) np array
+        """
+
+        k_tant = self.get_inv_dist_2d_k_ant(agent_id,
+                                             is_neigh=lambda id0, id1: True,
+                                             k=8,
+                                             spin=True)
+        return k_tant.reshape((-1, 1))
+
+
 class xyzBlimp(blimpNet):
     def __init__(self,
                  num_agents,
