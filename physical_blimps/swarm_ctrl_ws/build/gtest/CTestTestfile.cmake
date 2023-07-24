93250f8fef3dd2ac2f0a1535cba236b485647e2c
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2023-07-24 15:30:48 -0400

    Delete physical_blimps/swarm_ctrl_ws/build directory

--- physical_blimps/swarm_ctrl_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/rajbhandari/projects/swarm_coppeliasim/physical_blimps/swarm_ctrl_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
