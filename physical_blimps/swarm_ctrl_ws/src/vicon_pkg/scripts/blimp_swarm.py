b19241d8acaabf8d83a627948bf0b222a28da25f
god <prajbhan@andrew.cmu.edu>
2023-07-24 23:04:21 -0400

    physical experiments

--- physical_blimps/swarm_ctrl_ws/src/vicon_pkg/scripts/blimp_swarm.py
+++ physical_blimps/swarm_ctrl_ws/src/vicon_pkg/scripts/blimp_swarm.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python3
 from pyBlimp.blimp import BlimpManager
 from pyBlimp.utils import read_config
-from swarm_ctrl_ws.src.vicon_pkg.src.vicon import Vicon
+from swarm_ctrl_ws.src.vicon_pkg.scripts.vicon import Vicon
 from CONFIG import *
 import numpy as np
+import time
 
 
 class viconBlimps(BlimpManager):
     def __init__(self,
                  cfg_paths,
                  vicon: Vicon,  # IMPORTANT: vicon's object with id 'i' must correspond to blimp cfg_paths[i]
+                 command_type='force',
                  port=ALIEN_PORT,
-                 logger=False):
+                 logger=False,
+                 ):
         """
         BlimpManager with added vicon interface so we can grab the positions of the blimps as well
             THIS USES GLOBAL STUFF to make everything less of a headache
@@ -20,15 +24,41 @@ class viconBlimps(BlimpManager):
             we will call pyBlimp.utils.read_config on this list then pass it to BlimpManager
         @param vicon: a Vicon object
             @NOTE: vicon's object with id 'i' must correspond to blimp cfg_paths[i]
+        @param command_type: from ['position', 'velocitiy', 'force'], determines what the move_agent function does
         @param port: USB port that that alien thingy is plugged into
             this should be specified in CONFIG.py, and not really necessary to change
-        @param logger: probably what it sounds like
+        @param logger: hardly know her
         """
         cfg = read_config(cfg_paths)
         self.num_blimps = len(cfg_paths)
         super().__init__(cfg=cfg, port=port, logger=logger)
         self.vicon = vicon
+        self.change_command_type(command_type)
+        self.reset_agent_goals()
 
+    ####################################################################################################################
+    # init/shutdown functions
+    ####################################################################################################################
+    def reset_agent_goals(self):
+        """
+        resets agent goals if using position or velocity controller
+        """
+        self.agent_goals = [None for _ in range(self.num_blimps)]
+
+    def change_command_type(self, new_type):
+        """
+        changes command type to the desired
+
+        @param new_type: from ['position', 'velocitiy', 'force'], determines what the move_agent function does
+        """
+        if new_type not in {'position', 'velocitiy', 'force'}:
+            raise Exception("invalid command type: " + str(new_type))
+        self.command_type = new_type
+        self.reset_agent_goals()
+
+    ####################################################################################################################
+    # state access functions
+    ####################################################################################################################
     def get_position(self, agent_id):
         """
         returns position of blimp
@@ -45,6 +75,20 @@ class viconBlimps(BlimpManager):
         """
         return self.vicon.get_object_head(agent_id)
 
+    ####################################################################################################################
+    # command functions
+    ####################################################################################################################
+    def movement_updates(self):
+        """
+        updates the movements of agents if using velocity or position controllers
+            if the agents have not been asked to move, just make them still
+        """
+        for agent_id in range(self.num_blimps):
+            if self.agent_goals[agent_id] is None:
+                self.velocity_command(agent_id=agent_id, vec=np.zeros(3))
+            else:
+                self.move_agent(agent_id=agent_id, vec=self.agent_goals[agent_id])
+
     def move_agent(self, agent_id, vec):
         """
         moves an agent
@@ -52,7 +96,12 @@ class viconBlimps(BlimpManager):
         @param agent_id: agent id to publish to
         @param vec: vector to send to the method we decide to use
         """
-        return self.force_stability_command(agent_id=agent_id, vec=vec)
+        if self.command_type == 'position':
+            return self.position_command(agent_id=agent_id, vec=vec)
+        elif self.command_type == 'velocity':
+            return self.velocity_command(agent_id=agent_id, vec=vec)
+        elif self.command_type == 'force':
+            return self.force_stability_command(agent_id=agent_id, vec=vec)
 
     def position_command(self, agent_id, vec, update=True):
         """
@@ -65,7 +114,7 @@ class viconBlimps(BlimpManager):
 
         @note: if given an R^3 vector, we will let the turn command be to stabilize the blimp rotation
         """
-
+        self.agent_goals[agent_id] = vec
         pos = self.vicon.get_object_pos(obj_id=agent_id, update=update)
         theta = self.vicon.get_object_head(obj_id=agent_id, update=False)
         vel = vec[:3] - pos
@@ -86,6 +135,7 @@ class viconBlimps(BlimpManager):
 
         @note: if given an R^3 vector, we will let the turn command be to stabilize the blimp rotation
         """
+        self.agent_goals[agent_id] = vec
         vel = self.vicon.get_object_vel(obj_id=agent_id, update=update)
         omega = self.vicon.get_head_speed(obj_id=agent_id, update=False)
         acc = vec[:3] - vel
@@ -105,6 +155,7 @@ class viconBlimps(BlimpManager):
 
         @note: if given an R^3 vector, we will let the turn command be to stabilize the blimp rotation
         """
+        self.agent_goals[agent_id] = vec  # why not
         EFFECT = [1., 1., 1., 1.]  # effective force for x,y,z,rotation given a command
         # found experimentally
         xy = vec[:2]
@@ -139,3 +190,122 @@ class viconBlimps(BlimpManager):
         """
         vel = self.vicon.get_head_speed(obj_id=agent_id, update=update)
         return -vel
+
+
+class PhysicalExperiment(viconBlimps):
+    def __init__(self,
+                 cfg_paths,
+                 vicon: Vicon,
+                 command_type='velocity',
+                 port=ALIEN_PORT,
+                 logger=False):
+        """
+        Experiment skeleton using physical blimps
+
+        @param cfg_paths: list of config yaml file paths to interface the blimps
+            we will call pyBlimp.utils.read_config on this list then pass it to BlimpManager
+        @param vicon: a Vicon object
+            @NOTE: vicon's object with id 'i' must correspond to blimp cfg_paths[i]
+        @param command_type: from ['position', 'velocitiy', 'force'], determines what the move_agent function does
+        @param port: USB port that that alien thingy is plugged into
+            this should be specified in CONFIG.py, and not really necessary to change
+        @param logger: hardly know her
+        """
+        super().__init__(cfg_paths=cfg_paths,
+                         vicon=vicon,
+                         command_type=command_type,
+                         port=port,
+                         logger=logger)
+
+    ####################################################################################################################
+    # Expiriment functions (some need implementation in subclass)
+    ####################################################################################################################
+    def init_exp(self):
+        """
+        to be run at start of experiment, usually just resetting blimp positions
+        """
+        raise NotImplementedError()
+
+    def step(self):
+        """
+        step to take continuously during an experiment
+        (should probably include a pause, since this will be running continuously)
+
+        @return: boolean, whether or not experiment is done
+        """
+        raise NotImplementedError()
+
+    def run_exp(self):
+        """
+        runs a single expiriment trial
+
+        @return: returns result of self.goal_data
+        """
+        self.init_exp()
+        while self.step():
+            pass
+        output = self.goal_data()
+        return output
+
+    def experiments(self, trials):
+        """
+        runs multiple experiments, resetting scene at start of each one
+
+        @param trials: number of experiments to run
+        @return: returns list of results of self.goal_data for each trial
+        """
+        results = []
+        for trial in range(trials):
+            data = self.run_exp()
+            if data is None:
+                return None
+            results.append(data)
+        return results
+
+    def goal_data(self):
+        """
+        data to return at the end of each experiment trial
+
+        @return: can be anything, None signals error
+        """
+        raise NotImplementedError()
+
+    ####################################################################################################################
+    # utility functions
+    ####################################################################################################################
+    def set_up_agents(self, position_arrray, tolerance=.5, timeout=100):
+        """
+        Moves agents to specified locations
+        @param position_arrray: array of R^3 numpy arrays for each blimp (must be length of num_blimps)
+            if we dont care about a blimp, just use None instead of a position
+        @param tolerance: tolerance of position
+        @param timeout: seconds to try this before giving up
+        @return:
+        """
+        old_command_type = self.command_type
+        self.change_command_type('velocity')
+        start = time.time()
+        while time.time() - start < timeout:
+            done = True
+            for agent_id in range(self.num_blimps):
+                goal = position_arrray[agent_id]
+                if goal is None:
+                    self.move_agent(agent_id, np.zeros(3))
+                    continue
+                pos = self.get_position(agent_id=agent_id)
+                vec = goal - pos
+                if vec <= tolerance:
+                    self.move_agent(agent_id, np.zeros(3))
+                else:
+                    done = False
+                    self.move_agent(agent_id, vec)
+            if done:
+                break
+        for agent_id in range(self.num_blimps):
+            self.move_agent(agent_id, np.zeros(3))
+        self.change_command_type(old_command_type)
+
+
+if __name__ == "__main__":
+    test = PhysicalExperiment(cfg_paths=[create_config_file(2)], vicon=Vicon(['b2/b2']))
+    test.set_up_agents([np.array((0., 0., 1.))])
--- physical_blimps/swarm_ctrl_ws/src/vicon_pkg/scripts/vicon.py
+++ physical_blimps/swarm_ctrl_ws/src/vicon_pkg/scripts/vicon.py
@@ -6,39 +6,41 @@ import rospy
 from geometry_msgs.msg import TransformStamped
 from scipy.spatial.transform import Rotation as R
 
+
 class Vicon:
-    def __init__(self, topics,topic_global='/vicon/',queue_len=10):
+    def __init__(self, topics, topic_global='/vicon/', queue_len=3):
         """
         for interface with the Vicon system
             grabs object positions and headings
 
         """
         self.num_objects = len(topics)
-        self.blimps=[]
+        self.blimps = []
         rospy.init_node('listener_hardly_know_her')
-        for obj_id,local in enumerate(topics):
-            this_blimp=dict()
-            object_info={
+        for obj_id, local in enumerate(topics):
+            this_blimp = dict()
+            object_info = {
                 'time': -1,
                 'linear': {'pos': np.zeros(3), 'vel': np.zeros(3), 'acc': np.zeros(3)},
                 'angular': {'euler': np.zeros(3), 'vel': np.zeros(3), 'acc': np.zeros(3)},
-                'pos_hist':deque(maxlen=queue_len),
-                'ang_hist':deque(maxlen=queue_len),
-                'time_hist':deque(maxlen=queue_len),
+                'pos_hist': deque(maxlen=queue_len),
+                'ang_hist': deque(maxlen=queue_len),
+                'time_hist': deque(maxlen=queue_len),
             }
-            this_blimp['info']=object_info
-            topic=topic_global+local
-            this_blimp['topic']=topic
+            this_blimp['info'] = object_info
+            topic = topic_global + local
+            this_blimp['topic'] = topic
             self.blimps.append(this_blimp)
-            callback=self.create_subscriber_callback(dictionary=this_blimp,key='info')
-            rospy.Subscriber(local.replace('/','_'),TransformStamped,callback)
-        self.topics=[]
-        for obj_id,local in enumerate(topics):
-            topic=topic_global+local
+            callback = self.create_subscriber_callback(dictionary=this_blimp, key='info')
+            rospy.Subscriber(local.replace('/', '_'), TransformStamped, callback)
+        self.topics = []
+        for obj_id, local in enumerate(topics):
+            topic = topic_global + local
             self.topics.append(topic)
 
         # raise NotImplementedError()
-    def create_subscriber_callback(self,dictionary,key='info'):
+
+    def create_subscriber_callback(self, dictionary, key='info'):
         """
         creates a callback that adds to history of dictionary[key]
 
@@ -47,29 +49,28 @@ class Vicon:
         """
 
         def callback(msg):
-            pos=msg.transform.translation
-            quat=msg.transform.rotation
-            time=msg.header.stamp.secs+msg.header.stamp.nsecs*10**(-9)
-            skip=False
+            pos = msg.transform.translation
+            quat = msg.transform.rotation
+            time = msg.header.stamp.secs + msg.header.stamp.nsecs*10**(-9)
+            skip = False
             if dictionary[key]['time_hist']:
-                last_time=dictionary[key]['time_hist'][-1]
-                if time<=last_time:
-                    skip=True
-                    if time<last_time:
+                last_time = dictionary[key]['time_hist'][-1]
+                if time <= last_time:
+                    skip = True
+                    if time < last_time:
                         raise Exception("how did we get here")
-                    #return
+                    # return
             if not skip:
-                pos=np.array((pos.x,pos.y,pos.z))
+                pos = np.array((pos.x, pos.y, pos.z))
                 r = R.from_quat([quat.x, quat.y, quat.z, quat.w])
-                arr=r.as_euler('xyz', degrees=False)
+                arr = r.as_euler('xyz', degrees=False)
                 dictionary[key]['pos_hist'].append(pos)
                 dictionary[key]['ang_hist'].append(arr)
                 dictionary[key]['time_hist'].append(time)
+
         return callback
 
-
-
-    def update(self, obj_ids, pos_samples=1,vel_samples=1,acc_samples=1):
+    def update(self, obj_ids, pos_samples=1, vel_samples=1, acc_samples=1):
         """
         updates values for obj ids
         @param obj_ids: array of object ids to update values for
@@ -79,31 +80,44 @@ class Vicon:
             for angular, make sure you take the velocity/acceleration accounting for topology of circle
         """
         for obj_id in obj_ids:
-            object_info=self.blimps[obj_id]['info'].copy()
+            object_info = self.blimps[obj_id]['info'].copy()
             if not object_info['time_hist']:
-                print("WARNING: trying to access unpublished topic:",self.blimps[obj_id]['topic'])
+                print("WARNING: trying to access unpublished topic:", self.blimps[obj_id]['topic'])
                 continue
-            self.blimps[obj_id]['info']['time']=object_info['time_hist'][-1]
-            pos_samp=max(pos_samples,len(object_info['pos_hist']))
-            self.blimps[obj_id]['info']['linear']['pos']=np.mean([object_info['pos_hist'][-i] for i in range(1,pos_samp+1)],axis=0)
-            
+            self.blimps[obj_id]['info']['time'] = object_info['time_hist'][-1]
+            pos_samp = max(pos_samples, len(object_info['pos_hist']))
+            self.blimps[obj_id]['info']['linear']['pos'] = np.mean(
+                [object_info['pos_hist'][-i] for i in range(1, pos_samp + 1)], axis=0)
+
             # TODO: should we use angle averages
-            self.blimps[obj_id]['info']['angular']['euler']=object_info['ang_hist'][-1] 
-            
-            if len(object_info['time_hist'])>1 and vel_samples>0:
-                vel_samp=max(vel_samples,len(object_info['time_hist'])-1)
-                self.blimps[obj_id]['info']['linear']['vel']=np.mean([
-                                                                (object_info['pos_hist'][-i]-object_info['pos_hist'][-i-1])/(object_info['time_hist'][-i]-object_info['time_hist'][-i-1]) 
-                                                                for i in range(1,vel_samp+1)],axis=0)
-                self.blimps[obj_id]['info']['angular']['vel']=(object_info['ang_hist'][-1]-object_info['ang_hist'][-2])/(object_info['time_hist'][-1]-object_info['time_hist'][-2])
-            if len(object_info['time_hist'])>2 and acc_samples>0:
-                acc_samp=max(acc_samples,len(object_info['time_hist'])-2)
-                velocities=[(object_info['pos_hist'][-i]-object_info['pos_hist'][-i-1])/(object_info['time_hist'][-i]-object_info['time_hist'][-i-1]) 
-                                                            for i in range(1,acc_samp+2)]
-                self.blimps[obj_id]['info']['linear']['acc']=np.mean([
-                                                                (velocities[-i]-velocities[-i-1])/(object_info['time_hist'][-i]-object_info['time_hist'][-i-1])
-                                                                for i in range(1,acc_samp+1)],axis=0)
-            
+            self.blimps[obj_id]['info']['angular']['euler'] = object_info['ang_hist'][-1]
+
+            if len(object_info['time_hist']) > 1 and vel_samples > 0:
+                vel_samp = max(vel_samples, len(object_info['time_hist']) - 1)
+                self.blimps[obj_id]['info']['linear']['vel'] = np.mean([
+                    (object_info['pos_hist'][-i] - object_info['pos_hist'][-i - 1])/(
+                            object_info['time_hist'][-i] - object_info['time_hist'][-i - 1])
+                    for i in range(1, vel_samp + 1)], axis=0)
+
+                self.blimps[obj_id]['info']['angular']['vel'] = self.angle_diff(object_info['ang_hist'][-1],
+                                                                                object_info['ang_hist'][-2])/(
+                                                                        object_info['time_hist'][-1] -
+                                                                        object_info['time_hist'][-2])
+            if len(object_info['time_hist']) > 2 and acc_samples > 0:
+                acc_samp = max(acc_samples, len(object_info['time_hist']) - 2)
+                velocities = [(object_info['pos_hist'][-i] - object_info['pos_hist'][-i - 1])/(
+                        object_info['time_hist'][-i] - object_info['time_hist'][-i - 1])
+                              for i in range(1, acc_samp + 2)]
+                self.blimps[obj_id]['info']['linear']['acc'] = np.mean([
+                    (velocities[-i] - velocities[-i - 1])/(
+                            object_info['time_hist'][-i] - object_info['time_hist'][-i - 1])
+                    for i in range(1, acc_samp + 1)], axis=0)
+
+                ang_velocities = [self.angle_diff(object_info['ang_hist'][-i], object_info['ang_hist'][-i - 1])/(
+                        object_info['time_hist'][-i] - object_info['time_hist'][-i - 1])
+                                  for i in range(1, 3)]
+                self.blimps[obj_id]['info']['angular']['acc'] = (ang_velocities[-1] - ang_velocities[-2])/(
+                        object_info['time_hist'][-1] - object_info['time_hist'][-2])
 
     def get_last_update_time(self, obj_id, update=True):
         if update:
@@ -202,19 +216,35 @@ class Vicon:
             self.update(obj_ids=[obj_id])
         return self.blimps[obj_id]['info']['angular']['acc']
 
-if __name__=="__main__":
-    x=Vicon(['chatter'],'')
+    @staticmethod
+    def angle_diff(a, b):
+        """
+        returns a-b on (-pi,pi]
+        @param a: angle a
+        @param b: angle b
+        @return: a-b
+        """
+        diff = (a - b)%(2*np.pi)
+        if diff > np.pi:
+            diff -= 2*np.pi
+        return diff
+
+
+if __name__ == "__main__":
+    x = Vicon(['chatter'], '')
     while True:
         time.sleep(.2)
         print()
         print('pos')
         print(x.get_object_pos(0))
         print('vel')
-        print(x.get_object_vel(0,False))
+        print(x.get_object_vel(0, False))
         print('acc')
-        print(x.get_object_acc(0,False))
+        print(x.get_object_acc(0, False))
         print()
     quit()
+
+
     def callback(data):
         rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data)
         print("nice")
@@ -226,11 +256,13 @@ if __name__=="__main__":
         # anonymous=True flag means that rospy will choose a unique
         # name for our 'listener' node so that multiple listeners can
         # run simultaneously.
-        
+
         rospy.init_node('listener', anonymous=True)
 
         rospy.Subscriber("chatter", TransformStamped, callback)
 
         # spin() simply keeps python from exiting until this node is stopped
         rospy.spin()
+
+
     listener()
